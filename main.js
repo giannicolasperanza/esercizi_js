a50d0e8467f0c85359c8b3d9822fc86d065b91ec
Giannicola Speranza <speranzagiannicola@gmail.com>
2025-03-11 17:46:45 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+
+let num1 = 24;
+let num2 = 35;
+
+console.log(`Il valore della variabile num1 è ${num1}`);
+console.log(`Il valore della variabile num2 è ${num2}`);
+
+let stringa =` "Mi piace JS" `;
+console.log(`Il valore della varibile stringa è ${stringa}`);
+
+
+stringa = ` "Mi correggo mi piace tantissimo JS" `
+
+console.log(stringa);
+
+const PIGRECO = 3.14159265359;
+console.log(`Il valore di PIGRECO è ${PIGRECO}`);
+
+PIGRECO = 5;
+
+console.log(PIGRECO); 
+// chiaramente errore!!
