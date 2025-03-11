a11ab7641937587e2d3c0d0cbab7ee0478511e32
Giannicola Speranza <speranzagiannicola@gmail.com>
2025-03-11 22:13:21 +0100

    quinto esercizio

--- main.js
+++ main.js
@@ -46,18 +46,33 @@ let anniAiCento = 100 - anni;
 
 console.log(`Hai ${anni} anni e ti mancano ${anniAiCento}  anni per compierne 100`);
 
-//  quarto esercizio
-console.log("--- inizio quarto esercizio ---");
-let scelta1 = Number(prompt("Scegli il primo numero"));
-let scelta2 = Number(prompt("Scegli il secondo numero"));
-let somma1 = scelta1 + scelta2;
-let sottrazione = scelta1 - scelta2;
-let moltiplicazione = scelta1 * scelta2;
-let divisione = scelta1 / scelta2;
-let potenza1 = scelta1 ** 2;
-let potenza2 = scelta2 ** 2;
+// //  quarto esercizio
+// console.log("--- inizio quarto esercizio ---");
+// let scelta1 = Number(prompt("Scegli il primo numero"));
+// let scelta2 = Number(prompt("Scegli il secondo numero"));
+// let somma1 = scelta1 + scelta2;
+// let sottrazione = scelta1 - scelta2;
+// let moltiplicazione = scelta1 * scelta2;
+// let divisione = scelta1 / scelta2;
+// let potenza1 = scelta1 ** 2;
+// let potenza2 = scelta2 ** 2;
 
-console.log(` Con i numeri da te scelti, i risultati delle varie operazioni sono : \n somma = ${somma1} ; \n sottrazione = ${sottrazione}; \n moltiplicazione = ${moltiplicazione} ; \n divisone = ${divisione} ; \n potenza alla seconda del primo numero scelto = ${potenza1} ;\n potenza alla seconda del secondo numero scelto = ${potenza2} \n
+// console.log(` Con i numeri da te scelti, i risultati delle varie operazioni sono : \n somma = ${somma1} ; \n sottrazione = ${sottrazione}; \n moltiplicazione = ${moltiplicazione} ; \n divisone = ${divisione} ; \n potenza alla seconda del primo numero scelto = ${potenza1} ;\n potenza alla seconda del secondo numero scelto = ${potenza2} \n`);
 
-    `);
+//  quinto esercizio
+console.log("--- inizio quinto  esercizio ---");
+
+let numeroGatti = 44;
+let numeroGattiPerFila = 10;
+
+let numeroFile = Math.floor(numeroGatti / numeroGattiPerFila);
+let gattiRimanenti = numeroGatti % numeroGattiPerFila;
+let avanzo = numeroGattiPerFila - gattiRimanenti;
+
+console.log(`Ci sono ${numeroFile} file di gatti e ne mancano ${gattiRimanenti} per una nuova fila, con un avanzo di ${avanzo}`);
+
+
+
+
+
 
